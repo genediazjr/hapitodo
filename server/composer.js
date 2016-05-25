17e2093851d85ab634647542c68eaeb6d2f460b8
Gene Diaz <genediazjr@gmail.com>
2016-05-25 20:36:48 +0800

    remove unused relative option

--- server/composer.js
+++ server/composer.js
@@ -1,8 +1,7 @@
 'use strict';
 
 const Glue = require('glue');
-const Path = require('path');
 const Manifest = require('./manifest');
-const options = { relativeTo: Path.resolve(__dirname, '..', 'plugin') };
+const options = {};
 
 module.exports = Glue.compose.bind(Glue, Manifest.get('/'), options);
